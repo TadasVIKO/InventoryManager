5ba7255d50ff5c440d9910d214627aa6b0d2a057
TadasVIKO <tadas.grigaitis@stud.viko.lt>
2024-10-12 14:40:36 +0300

    Test commit

--- src/main/java/lt/bropro/inventorymanager/server/Main.java
+++ src/main/java/lt/bropro/inventorymanager/server/Main.java
@@ -15,6 +15,7 @@ public class Main {
      * @param args command-line arguments
      */
     public static void main(String[] args) {
+        // COMMENT FOR TEST!
         SpringApplication.run(Main.class, args);
     }
 
